314bafd7370b9501c5156684defe57610eddb282
Surubariu Razvan <jdery99@gmail.com>
2021-03-06 01:24:17 +0200

    pizza Project

--- Docs/Lab01/src/test/java/org/vvss/AppTest.java
+++ Docs/Lab01/src/test/java/org/vvss/AppTest.java
@@ -1,20 +1,18 @@
 package org.vvss;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
 }
