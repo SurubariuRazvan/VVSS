55d7a49609eb128da72496b0bc528a2581348d9e
Surubariu Razvan <jdery99@gmail.com>
2021-03-06 15:26:34 +0200

    pizza Project

--- Docs/Lab01/src/main/java/pizzashop/repository/PaymentRepository.java
+++ Docs/Lab01/src/main/java/pizzashop/repository/PaymentRepository.java
@@ -1,16 +1,11 @@
 package pizzashop.repository;
 
+import javafx.collections.ObservableList;
+import pizzashop.model.MenuDataModel;
 import pizzashop.model.Payment;
 import pizzashop.model.PaymentType;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -19,45 +14,58 @@ public class PaymentRepository {
 	private static String filename = "data/payments.txt";
 	private List<Payment> paymentList;
 
-	public PaymentRepository() {
+	public PaymentRepository(){
 		this.paymentList = new ArrayList<>();
 		readPayments();
 	}
 
-	private void readPayments() {
-		URL resource = getClass().getClassLoader().getResource(filename);
+	private void readPayments(){
+		ClassLoader classLoader = PaymentRepository.class.getClassLoader();
+		File file = new File(classLoader.getResource(filename).getFile());
+		BufferedReader br = null;
 		try {
-			Files.readAllLines(Paths.get(resource.toURI())).forEach((line -> paymentList.add(getPayment(line))));
-		} catch (IOException | URISyntaxException e) {
+			br = new BufferedReader(new FileReader(file));
+			String line = null;
+			while((line=br.readLine())!=null){
+				Payment payment=getPayment(line);
+				paymentList.add(payment);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private Payment getPayment(String line) {
-		Payment item = null;
-		if (line == null || line.equals(""))
-			return null;
-		StringTokenizer st = new StringTokenizer(line, ",");
-		int tableNumber = Integer.parseInt(st.nextToken());
-		String type = st.nextToken();
+	private Payment getPayment(String line){
+		Payment item=null;
+		if (line==null|| line.equals("")) return null;
+		StringTokenizer st=new StringTokenizer(line, ",");
+		int tableNumber= Integer.parseInt(st.nextToken());
+		String type= st.nextToken();
 		double amount = Double.parseDouble(st.nextToken());
 		item = new Payment(tableNumber, PaymentType.valueOf(type), amount);
 		return item;
 	}
 
-	public void add(Payment payment) {
+	public void add(Payment payment){
 		paymentList.add(payment);
 		writeAll();
 	}
 
-	public void writeAll() {
+	public List<Payment> getAll(){
+		return paymentList;
+	}
+
+	public void writeAll(){
 		ClassLoader classLoader = PaymentRepository.class.getClassLoader();
 		File file = new File(classLoader.getResource(filename).getFile());
 
 		BufferedWriter bw = null;
 		try {
 			bw = new BufferedWriter(new FileWriter(file));
-			for (Payment p : paymentList) {
+			for (Payment p:paymentList) {
 				System.out.println(p.toString());
 				bw.write(p.toString());
 				bw.newLine();
@@ -66,10 +74,6 @@ public class PaymentRepository {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public List<Payment> getAll() {
-		return paymentList;
 	}
 
 }
