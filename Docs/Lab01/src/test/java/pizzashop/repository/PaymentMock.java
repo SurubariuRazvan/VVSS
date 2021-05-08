b8fb1c199960947d85beb79c355769a7f3eb666f
Surubariu Razvan <jdery99@gmail.com>
2021-05-08 14:47:32 +0300

    added tests for code coverage

--- Docs/Lab01/src/test/java/pizzashop/repository/PaymentMock.java
+++ Docs/Lab01/src/test/java/pizzashop/repository/PaymentMock.java
@@ -1,32 +1,34 @@
-package pizzashop.model;
+package pizzashop.repository;
 
-public class Payment {
+import pizzashop.model.Payment;
+import pizzashop.model.PaymentType;
 
+public class PaymentMock extends Payment {
 	private int tableNumber;
 	private PaymentType type;
 	private double amount;
 	
-	public Payment() {
-	}
-	
-	public Payment(int tableNumber, PaymentType type, double amount) {
+	public PaymentMock(int tableNumber, PaymentType type, double amount) {
 		this.tableNumber = tableNumber;
 		this.type = type;
 		this.amount = amount;
 	}
-
+	
+	@Override
 	public int getTableNumber() {
 		return tableNumber;
 	}
-
+	
+	@Override
 	public PaymentType getType() {
 		return type;
 	}
-
+	
+	@Override
 	public double getAmount() {
 		return amount;
 	}
-
+	
 	@Override
 	public String toString() {
 		return tableNumber + "," + type + "," + amount;
